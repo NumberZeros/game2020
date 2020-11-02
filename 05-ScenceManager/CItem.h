b82c95b24e1b5bf4490abb9b0ff8e1ae51018469
phanletrongnhan <58338443+phanletrongnhan@users.noreply.github.com>
2020-11-02 14:12:38 +0700

    init good

---
+++ 05-ScenceManager/CItem.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "GameObject.h"
+
+#define ITEM_DISAPPEAR_TIME		5000
+class CItem :
+	public CGameObject
+{
+protected:
+	DWORD disappearStart;
+public:
+	CItem();
+	~CItem();
+
+	void StartDisappear();
+
+	virtual void GetBoundingBox(float& l, float& t, float& r, float& b) = 0;
+	virtual void Render();
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+};
+
